bdc09b6007255f3cdfc7e0e51180fc68d095ec87
Matthew Allen <matthewallenunc@gmail.com>
2019-04-06 02:21:20 -0400

    attackers and defenders go where they are supposed to now!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -32,33 +32,60 @@ $(document).ready(function() {
 
 var currCharacterSelected
 var combatants = [];
+var currDefender;
 
-var renderOne = function(character, renderArea) {
+var renderOne = function(character, renderArea, charStatus) {
     var charDiv = $("<div class='character' data-name='" + character.name + "'>");
     var charName = $("<div class='character-name'>").text(character.name);
     var charImage = $("<img alt='image' class='character-image'>").attr("src", character.imageUrl);
     var charHealth = $("<div class='character-health'>").text(character.health);
     charDiv.append(charName).append(charImage).append(charHealth);
     $(renderArea).append(charDiv);
-}
+
+    if (charStatus === "enemy") {
+        $(charDiv).addClass("enemy");
+    }
+    else if (charStatus === "defender") {
+        currDefender = character;
+        $(charDiv).addClass("target-enemy");
+    }
+};
 
     var renderCharacters = function(charObj, areaRender) {
         if (areaRender === "#character-section") {
             $(areaRender).empty();
             for(var key in charObj) {
                 if(charObj.hasOwnProperty(key)) {
-                    renderOne(charObj[key], areaRender);
+                    renderOne(charObj[key], areaRender, "");
                 }
             }
         }
 
         if (areaRender === "#selected-character") {
-            renderOne(charObj, areaRender);
+            renderOne(charObj, areaRender, "");
         }
 
         if (areaRender === "#available-to-attack-section") {
             for (var i = 0; i < charObj.length; i++) {
-                renderOne(charObj[i], areaRender);
+                renderOne(charObj[i], areaRender, "enemy");
+            }
+
+            $(document).on("click", ".enemy", function() {
+                var name = ($(this).attr("data-name"));
+
+                if ($("#defender").children().length === 0) {
+                    renderCharacters(name, "#defender");
+                    $(this).hide();
+                }
+            });
+        }
+
+        if (areaRender === "#defender") {
+            $(areaRender).empty();
+            for (var i = 0; i < combatants.length; i++) {
+                if (combatants[i].name === charObj) {
+                    renderOne(combatants[i], areaRender, "defender");
+                }
             }
         }
     }
